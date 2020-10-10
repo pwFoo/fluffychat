9622dd89aa0712a47b18e1989031d1daf8d4bc61
Lukas Lihotzki <lukas@lihotzki.de>
2020-10-10 19:11:23 +0200

    Enable Wayland support

--- linux/main.cc
+++ linux/main.cc
@@ -1,9 +1,7 @@
 #include "my_application.h"
 
 int main(int argc, char** argv) {
-  // Only X11 is currently supported.
-  // Wayland support is being developed: https://github.com/flutter/flutter/issues/57932.
-  gdk_set_allowed_backends("x11");
+  // Allow all GDK backends (including Wayland).
 
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
