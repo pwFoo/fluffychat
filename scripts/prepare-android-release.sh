b5beec7e111d09dc6d0d5126125304f7b171dae7
Christian Pauly <christian-pauly@posteo.de>
2020-11-22 09:06:07 +0100

    chore: Implement playstore CD

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -4,4 +4,4 @@ cd android && echo "storePassword=${FDROID_KEY_PASS}" >> key.properties && cd ..
 cd android && echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties && cd ..
 cd android && echo "keyAlias=key" >> key.properties && cd ..
 cd android && echo "storeFile=../key.jks" >> key.properties && cd ..
-cd android/app && echo $GOOGLE_SERVICES >> google-services.json && cd ../..
+#cd android/app && echo $GOOGLE_SERVICES >> google-services.json && cd ../..
--- scripts/release-playstore-beta.sh
+++ scripts/release-playstore-beta.sh
@@ -2,6 +2,15 @@
 flutter channel stable
 flutter upgrade
 flutter pub get
+cd android
+bundle install
+bundle update fastlane
+#echo $PLAYSTORE_DEPLOY_KEY >> keys.json
+bundle exec fastlane set_build_code_beta
+cd ..
 flutter build appbundle --target-platform android-arm,android-arm64,android-x64
 mkdir -p build/android
 cp build/app/outputs/bundle/release/app-release.aab build/android/
+cd android
+bundle exec fastlane deploy_beta_test
+cd ..
